de748b173daa0098dd0059697520a6c5e8bf52cd
Evgeny Kashirin <evgeny.kashirin@cern.ch>
2020-09-01 11:47:42 +0300

    Mimicking centrality task

--- sample_task/SampleTask.h
+++ sample_task/SampleTask.h
@@ -11,6 +11,7 @@
 
 #include <core/Detector.hpp>
 #include <core/Module.hpp>
+#include <core/EventHeader.hpp>
 
 class FooTask : public UserTask {
 public:
@@ -20,23 +21,33 @@ public:
   }
   void PreInit() override {
     /* insert your pre-init here */
-    SetInputBranchNames({"PsdModules"});
+    SetInputBranchNames({"PsdModules", "SimEventHeader"});
+    SetOutputBranchName("Centality");
   }
 
   void Init(std::map<std::string, void *> &branches_map) override {
     psd = static_cast<ModuleDetector *>(branches_map.at("PsdModules"));
     in_chain_->SetBranchAddress("PsdModules", &psd);
+
+    AnalysisTree::BranchConfig centrality_branch(out_branch_, AnalysisTree::DetType::kEventHeader);
+    centrality_branch.AddField<float>("Centrality_Epsd");
+    centrality_Epsd_field_id = centrality_branch.GetFieldId("Centrality_Epsd");
+
+    out_tree_->Branch(out_branch_.c_str(), "AnalysisTree::Container", &ana_event_header_);
+    out_config_->AddBranchConfig(centrality_branch);
+    ana_event_header_->Init(centrality_branch);
   }
 
   void Exec() override {
     auto n_channel = psd->GetNumberOfChannels();
 
+    float total_signal = 0.;
     for (int ich = 0; ich < n_channel; ++ich) {
       auto channel = psd->GetChannel(ich);
       auto signal = channel.GetSignal();
-      std::cout << signal << std::endl;
+      total_signal += signal;
     }
-
+    ana_event_header_->SetField(total_signal, centrality_Epsd_field_id);
 
   }
   void Finish() override {
@@ -46,10 +57,11 @@ public:
 private:
   typedef AnalysisTree::Detector<AnalysisTree::Module> ModuleDetector;
 
-  ModuleDetector *psd {nullptr};
+  AnalysisTree::Container *ana_event_header_{nullptr};
+  ModuleDetector *psd{nullptr};
+  int centrality_Epsd_field_id{-999};
 
 TASK_DEF(FooTask)
 };
-
 
 #endif //ANALYSISTREESKELETON_SAMPLE_TASK_SAMPLETASK_H
